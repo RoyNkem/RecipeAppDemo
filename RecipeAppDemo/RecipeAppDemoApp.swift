c166e8cbd533aebf35d6f94600a84797f988ebab
Aiyetin Roy <aiyetinroy@gmail.com>
2022-05-12 18:42:29 +0100

    Initial Commit

---
+++ RecipeAppDemo/RecipeAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppDemoApp.swift
+//  RecipeAppDemo
+//
+//  Created by Roy Aiyetin on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
